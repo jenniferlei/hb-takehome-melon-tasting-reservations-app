6384247703266462c4680845b1b683e53e43e7e9
Jennifer Lei <jenniferlei1928@gmail.com>
2022-04-01 22:40:55 +0000

    Updated model.py

--- model.py
+++ model.py
@@ -39,7 +39,7 @@ class Reservation(db.Model):
         return f"<Reservation reservation_id={self.reservation_id} username={self.username} date={self.date} start_time={self.start_time} end_time={self.end_time}>"
 
 
-def connect_to_db(flask_app, db_uri="postgresql://evseexcyzaukhk:920fdfe2d7b0cb909504ad61587075e1fed59b38b12a5a949cd17d02755bad73@ec2-3-217-251-77.compute-1.amazonaws.com:5432/dcvr918o9v4v2d", echo=True):
+def connect_to_db(flask_app, db_uri="postgresql://hjswtvtyweofmp:997afb2da5bf488937b9f0cb220525037f574edc172a8ca809e8fc983153dbad@ec2-52-201-124-168.compute-1.amazonaws.com:5432/d7euob16cqobl1", echo=True):
     """connect to database"""
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
